3499d12175461d88c0a055afdb19e974c1345076
mint723 <neu990112@gmail.com>
2025-11-13 15:14:36 +0900

    refactor: JWT 토큰 / 시큐리티 설정 제거

--- src/main/java/com/agent/monito/MonitoApplication.java
+++ src/main/java/com/agent/monito/MonitoApplication.java
@@ -3,9 +3,7 @@ package com.agent.monito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication
 public class MonitoApplication {
 
 	public static void main(String[] args) {
